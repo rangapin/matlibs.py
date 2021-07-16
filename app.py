d0470add0daf1d8686c3fa8a4a64a610a55f065a
Richard Angapin <rangapin@gmx.com>
2021-07-16 03:05:32 +0200

    python first step

---
+++ app.py
@@ -0,0 +1,7 @@
+verb = input("Please enter a verb: ")
+noun = input("Please enter a noun: ")
+adjective = input("Please enter a adjective: ")
+
+print("I enjoy practice, I find it helps me to", verb, "better.")
+print("without practice, my", noun, "would probably not even work.")
+print("My code is getting more", adjective, "every single day!")
